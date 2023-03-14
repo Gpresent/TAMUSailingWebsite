9c46e6d85cef869619f974e280c72853d006d281
Gpresent <90751134+Gpresent@users.noreply.github.com>
2023-03-14 02:09:36 -0500

    Add files via upload
    All of the progress so far

---
+++ script.js
@@ -0,0 +1,21 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            if (!(entry.target.classList.contains('hiddenTitle'))) entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenTitle = document.querySelector('.hiddenTitle');
+observer.observe(hiddenTitle);
+
+const hiddenLeft = document.querySelectorAll('.left');
+hiddenLeft.forEach((el) => observer.observe(el));
+
+const hiddenRight = document.querySelectorAll('.right');
+hiddenRight.forEach((el) => observer.observe(el));
